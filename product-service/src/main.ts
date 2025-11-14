09e527d922e1d88a8103e9820cac35473ffdfc5c
Zaheed <zaheedshaik3@gmail.com>
2025-11-14 09:07:04 +0530

    Update main.ts

--- product-service/src/main.ts
+++ product-service/src/main.ts
@@ -1,1 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      host: 'localhost',
+      port: 3001,
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(3000);
+  console.log('✅ Product Service running on http://localhost:3000');
+}
+bootstrap();
